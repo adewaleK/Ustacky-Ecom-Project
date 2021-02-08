6e3ff8921add2c69cfb8ac3a90a0d70df09ebe46
unknown <adewaleK>
2021-02-08 03:19:56 +0100

    IMPLEMENTS MOBILE RESPONSIVE

---
+++ cart.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+        id: 1,
+        title: "SAMSUNG TV",
+        image: "product1.png",
+        price: "500,000"
+    },
+    {
+        id: 2,
+        title: "PIXEL 4a",
+        image: "product2.png",
+        price: "300,000"
+    },
+    {
+        id: 3,
+        title: "PS 5",
+        image: "product3.png",
+        price: "250,000"
+    },
+    {
+        id: 4,
+        title: "MACBOOK AIR",
+        image: "product4.png",
+        price: "400,000"
+    },
+    {
+        id: 5,
+        title: "APPLE WATCH",
+        image: "product5.png",
+        price: "200,000"
+    },
+    {
+        id: 6,
+        title: "AIR PODS",
+        image: "product6.png",
+        price: "300,000"
+    },
+];
+
+
+function loadProducts(){
+    products.map(product => {
+        var products = document.getElementById('products'); 
+        products.classList.add("products"); 
+        products.innerHTML += `
+            <div class="product-card" id="product-box"> 
+                <div class="prod${product.id}-img prod${product.id}-details prod-feature">
+                    <p class="price">Price</p>
+                    <p>&#8358;${product.price}</p>   
+                </div>
+                <h3 id="title">${product.title}</h3>
+                <button type="button" class="add-to-cart" data-id="${product.id}" >ADD TO CART</button>
+            </div>
+            `; 
+        })
+    }
+
+    function hoverEffect(x){
+        //when hover on product card x
+           x.classList.add(opaque);
+           x.style.color ="#fff";
+           //apply opaque BG style to x
+           //then change 'color' of product-details to #fff
+    }
+
+    var card = document.getElementById('product-box');
+
+
+window.onload = loadProducts;
