c2aee02bc6f46fa85115e1d7c36bc476c3cf882b
unknown <adewaleK>
2021-02-12 03:08:46 +0100

    IMPLEMENTS ADD TO CART FUNCTIONALITY AND DISPLAY CART ITEMS

--- cart.js
+++ cart.js
@@ -3,42 +3,42 @@ const products = [
         id: 1,
         title: "SAMSUNG TV",
         image: "product1.png",
-        price: "500,000",
+        price: 500000,
         quantity:1
     },
     {
         id: 2,
         title: "PIXEL 4a",
         image: "product2.png",
-        price: "300,000",
+        price: 300000,
         quantity:1
     },
     {
         id: 3,
         title: "PS 5",
         image: "product3.png",
-        price: "250,000",
+        price: 250000,
         quantity:1
     },
     {
         id: 4,
         title: "MACBOOK AIR",
         image: "product4.png",
-        price: "400,000",
+        price: 400000,
         quantity:1
     },
     {
         id: 5,
         title: "APPLE WATCH",
         image: "product5.png",
-        price: "200,000",
+        price: 200000,
         quantity:1
     },
     {
         id: 6,
         title: "AIR PODS",
         image: "product6.png",
-        price: "300,000",
+        price: 300000,
         quantity:1
     },
 ];
@@ -55,19 +55,36 @@ function loadProducts(){
                     <p>&#8358;${product.price}</p>   
                 </div>
                 <h3 id="title">${product.title}</h3>
-                <button type="button" class="add-to-cart" data-id="${product.id}" >ADD TO CART</button>
+                <button type="button" class="add-to-cart" data-id="${product.id}">ADD TO CART</button>
             </div>
-            `; 
-        })
-    }
+        `; 
+    })
+}
 
 
-window.onload = loadProducts;
+//window.onload = loadProducts;
+//document.addEventListener("DOMContentLoaded", loadProducts);
+//document.addEventListener("DOMContentLoaded", addToCart);
+document.addEventListener("DOMContentLoaded", function(){
+    loadProducts();
+    addToCart();
+//displayCart();
+   
+
+
+});
+
+//document.addEventListener("DOMContentLoaded",  displayCart);
+
+
+
+
 
 let modalBg = document.querySelector('.modal-bg');
 let cartBtn = document.querySelector('.cart');
 cartBtn.addEventListener('click', cartModal);
 window.addEventListener('click', removeModal)
+
 
 function cartModal(){
     modalBg.classList.add('active-bg');
@@ -78,3 +95,66 @@ function removeModal(event){
        modalBg.classList.remove('active-bg');
     }
 }
+
+//add to cart functionality
+   //Click on Add to Cart Button
+      //get the product through its ID
+      //push the product into an empty cart array
+
+      //Change background of Add to cart button
+      //Display the product in the cart
+
+      var btns = document.querySelectorAll(".add-to-cart");
+      var btn_arr = Array.from(btns);
+    //   btns.forEach(function(bt){
+    //       bt.addEventListener('click', function(e){
+    //           console.log(bt);
+    //       })
+    //   });
+    //console.log(btn_arr);
+    
+    let myCart = [];
+    function addToCart(){
+        var btns = document.querySelectorAll(".add-to-cart");
+        var btns_arr = Array.from(btns);
+        btns_arr.forEach(function(btn){
+            var prod = products.filter(p => p.id == parseInt(btn.dataset.id));
+            btn.addEventListener('click', function(e){     
+                if (!myCart.includes(prod)){       
+                    myCart.push(prod);
+                    displayCart(prod[0]);
+                }
+                
+            })     
+        });
+        
+    }
+    
+    var id = 0;
+    
+    var totalPrice=0;
+    function displayCart(pd){ 
+        
+        id++;
+        //myCart.map(item =>{
+          var cartBox = document.getElementById('cart-list');
+            cartBox.innerHTML += `
+                <tr>
+                <td>${id}</td>
+                <td>${pd.title}</td>
+                <td>${pd.price}</td>
+                <td class="qty-btn">
+                    <button type="button"> - </button>
+                    <span>${pd.quantity}</span>
+                    <button type="button"> + </button>
+                </td>
+                <td><button type="button" class="remove-btn" data-id="${pd.id}">Remove</button></td>
+                </tr>
+            `;
+
+            totalPrice += pd.price * pd.quantity;
+            console.log(totalPrice);
+           
+         //})
+    }
+    
