5de5afb7c4aa7931787594a3efca0b6fb25c1ff3
unknown <adewaleK>
2021-02-16 01:53:51 +0100

    IMPLEMENTS PRODUCT REMOVAL FROM CART-DOM

--- cart2.js
+++ cart2.js
@@ -46,10 +46,12 @@ products = [
 
 //Variables
 var id = 0;
+var quantity=1;
 var totalPrice=0;
 let modalBg = document.querySelector('.modal-bg');
+let actionBtns;
 let cartBtn = document.querySelector('.cart');
-var inc = document.getElementById('inc');
+var inc= document.getElementById('inc');
 var dec = document.getElementById('dec');
 var btns;
 var btns_arr;
@@ -60,6 +62,8 @@ var backToShop = document.getElementById('continue');
 var cartBox = document.getElementById('cart-list');
 var total = document.getElementById('total-price');
 var productsCon = document.getElementById('products'); 
+var revList = document.getElementById('cart-list');
+var qtyBtns;
 
 //Load and Display All Shop Products
 function loadProducts(){
@@ -83,6 +87,10 @@ function loadProducts(){
 //Display Add to Cart Modal
 function cartModal(){
     modalBg.classList.add('active-bg');
+    //console.log(revList);
+    removeFromDom();
+    inc();
+    
 }
 
 //Dismiss Add to Cart Modal
@@ -99,12 +107,12 @@ function addToCart(item, bt){
         displayCart(item);
         bt.innerHTML = 'REMOVE FROM CART'; 
         bt.classList.add('remove-from-cart');  
-        bt.style.backgroundColor = '#FFE9D6';
-        
+        bt.style.backgroundColor = '#FFE9D6';        
     }else{
         removeFromCart(item);
         bt.classList.remove('remove-from-cart');  
         bt.style.backgroundColor = '#FF9A3D';
+        bt.style.outline = 'none';
         bt.innerHTML = 'ADD TO CART';
     }
 }
@@ -118,43 +126,73 @@ function displayCart(pd){
             <td>${pd.title}</td>
             <td>${pd.price}</td>
             <td class="qty-btn">
-                <button type="button" id="dec"> - </button>
+                <button type="button" id="dec" class="action-btns"> - </button>
                 <span>${pd.quantity}</span>
-                <button type="button" id="inc" onclick="adjustQty"> + </button>
+                <button type="button" id="inc" class="action-btns"> + </button>
             </td>
             <td><button type="button" class="remove-btn" data-id="${pd.id}">Remove</button></td>
             </tr>
         `;
 
         sum(myCart);
+        actionBtns = document.querySelectorAll('.action-btns');
         removeBtns = document.querySelectorAll(".remove-btn");
+        qtyBtns = document.getElementById('qty-btn');
         removeArr = Array.from(removeBtns);
-        console.log(removeArr);
-    }
+        actionBtnsArr = Array.from(actionBtns);
+}
 
 //Remove an Item from Cart
 function removeFromCart(item){
    var itemsLeft;
    for (var i = 0; i < myCart.length; i++) { 
         if (myCart[i] === item) { 
-            myCart.splice(i, 1); 
+            var removed = myCart.splice(i, 1); 
+            console.log(removed);
             itemsLeft = myCart;
             sum(itemsLeft);
         } 
-    } 
+    }  
+
+}
+
+//Remove Cart Items From DOM and Update Price
+function removeFromDom(){
+    revList.addEventListener('click', function(e){
+        if(e.target.className == 'remove-btn'){
+            var row = e.target.parentElement.parentElement; 
+            revList.removeChild(row);
+            var toRemove = myCart.filter(prod=>prod.id == e.target.dataset.id);
+            console.log(toRemove[0]);
+            removeFromCart(toRemove[0]);
+        }   
+    })
 }
 
 //Get Total Price of Cart Items
 function sum(arr){
     let sum = 0;
     for(let i=0; i<arr.length; i++){
-         sum += arr[i].price;
+        sum += arr[i].price * quantity;
     }
-    
     total.innerHTML = sum;
     document.getElementById('num').innerHTML = arr.length;
     return sum;
+}
 
+//Increase Quantity
+function inc(){
+  revList.addEventListener('click', function(e){
+    //   if(e.target.className == ){
+
+    //   }
+    console.log(e.target);
+  })
+}
+
+//Decrease Quantity
+function dec(){
+    
 }
 
 //Continue Shopping
@@ -165,8 +203,9 @@ function continueShopping(){
 //Event Listeners
 document.addEventListener('DOMContentLoaded', function(){
     
+    
     loadProducts();
-
+    
     cartBtn.addEventListener('click', cartModal);
 
     modalBg.addEventListener('click', removeModal);
@@ -178,15 +217,11 @@ document.addEventListener('DOMContentLoaded', function(){
         btn.addEventListener('click', ()=>addToCart(prod[0],btn));
     }); 
 
-    removeBtns.forEach(function(btn){
+    removeArr.forEach(function(btn){
         var prod = myCart.filter(p => p.id == parseInt(btn.dataset.id));
         btn.addEventListener('click', ()=>removeFromCart(prod[0]));
-        console.log(btn);
-        // btn.addEventListener('click', function(e){
-        //     console.log(e.target);
-        //     removeBtns(prod[0]);
-        // });
     }); 
-    
-    
+
+  
+     
 })
