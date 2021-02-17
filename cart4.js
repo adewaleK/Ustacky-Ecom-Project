c97af387323eceaff795dfdf6961f9a91ce132b6
unknown <adewaleK>
2021-02-17 21:37:42 +0100

    MOIFIES ERRORS IN REMOVING ITEMS FROM CART

--- cart4.js
+++ cart4.js
@@ -1,46 +1,41 @@
 //All Products List
 products = [
-    {   index:1,
+    {
         id: 1,
         title: "SAMSUNG TV",
         image: "product1.png",
         price: 500000,
         quantity:1
     },
-    {   
-        index:2,
+    {
         id: 2,
         title: "PIXEL 4a",
         image: "product2.png",
         price: 300000,
         quantity:1
     },
-    {   
-        index:3,
+    {
         id: 3,
         title: "PS 5",
         image: "product3.png",
         price: 250000,
         quantity:1
     },
-    {   
-        index: 4,
+    {
         id: 4,
         title: "MACBOOK AIR",
         image: "product4.png",
         price: 400000,
         quantity:1
     },
-    {   
-        index: 5,
+    {
         id: 5,
         title: "APPLE WATCH",
         image: "product5.png",
         price: 200000,
         quantity:1
     },
-    {   
-        index: 6,
+    {
         id: 6,
         title: "AIR PODS",
         image: "product6.png",
@@ -56,14 +51,15 @@ var totalPrice=0;
 let modalBg = document.querySelector('.modal-bg');
 let actionBtns;
 let cartBtn = document.querySelector('.cart');
+var inc= document.getElementById('inc');
+var dec = document.getElementById('dec');
 var btns;
-//var btns = document.querySelectorAll(".add-to-cart");
 var btns_arr;
 var removeBtns;
 var removeArr;
 var myCart = [];
 var backToShop = document.getElementById('continue');
-//var cartBox = document.getElementById('cart-list');
+var cartBox = document.getElementById('cart-list');
 var total = document.getElementById('total-price');
 var productsCon = document.getElementById('products'); 
 var revList = document.getElementById('cart-list');
@@ -71,10 +67,9 @@ var qtyBtns;
 
 //Load and Display All Shop Products
 function loadProducts(){
-    let res='';
-    productsCon.classList.add("products"); 
-    products.map(product => {   
-        res += `
+    products.map(product => {
+        productsCon.classList.add("products"); 
+        productsCon.innerHTML += `
             <div class="product-card" id="product-box"> 
                 <div class="prod${product.id}-img prod${product.id}-details prod-feature">
                     <p class="price">Price</p>
@@ -84,12 +79,9 @@ function loadProducts(){
                 <button type="button" class="add-to-cart" data-id="${product.id}">ADD TO CART</button>
             </div>
         `; 
-
-        productsCon.innerHTML = res; 
     })
 
     btns = document.querySelectorAll(".add-to-cart");
-    btns_arr = Array.from(btns);
 }
 
 //Display Add to Cart Modal
@@ -97,7 +89,8 @@ function cartModal(){
     modalBg.classList.add('active-bg');
     //console.log(revList);
     removeFromDom();
-    //inc();
+    inc();
+    
 }
 
 //Dismiss Add to Cart Modal
@@ -111,17 +104,12 @@ function removeModal(event){
 function addToCart(item, bt){     
     if (!myCart.includes(item)){       
         myCart.push(item);
-        //displayCart(item);
-        showCartItems(myCart);
+        displayCart(item);
         bt.innerHTML = 'REMOVE FROM CART'; 
         bt.classList.add('remove-from-cart');  
         bt.style.backgroundColor = '#FFE9D6';        
     }else{
-        let rem;
-        //rem = removeFromCart(item);
         removeFromCart(item);
-        //showCartItems(rem);
-        //console.log(rem);
         bt.classList.remove('remove-from-cart');  
         bt.style.backgroundColor = '#FF9A3D';
         bt.style.outline = 'none';
@@ -138,9 +126,9 @@ function displayCart(pd){
             <td>${pd.title}</td>
             <td>${pd.price}</td>
             <td class="qty-btn">
-                <button type="button" id="dec" class="dec-btn" data-id="${pd.id}"> - </button>
+                <button type="button" id="dec" class="action-btns"> - </button>
                 <span>${pd.quantity}</span>
-                <button type="button" id="inc" class="inc-btn" data-id="${pd.id}"> + </button>
+                <button type="button" id="inc" class="action-btns"> + </button>
             </td>
             <td><button type="button" class="remove-btn" data-id="${pd.id}">Remove</button></td>
             </tr>
@@ -156,81 +144,28 @@ function displayCart(pd){
 
 //Remove an Item from Cart
 function removeFromCart(item){
-   let itemsLeft;
-   for (let i = 0; i < myCart.length; i++) { 
+   var itemsLeft;
+   for (var i = 0; i < myCart.length; i++) { 
         if (myCart[i] === item) { 
-            let removed = myCart.splice(i, 1); 
-            
+            var removed = myCart.splice(i, 1); 
+            console.log(removed);
             itemsLeft = myCart;
             sum(itemsLeft);
-
-            if(itemsLeft.length==0){
-                revList.innerHTML='';
-            }
-            showCartItems(itemsLeft);
         } 
     }  
-    return itemsLeft;
-}
 
-
-//Update Cart DOM when Cart Items Are Removed from SHOP
-
-function showCartItems(items){
-    let result='';
-    items.forEach((c,i) => {
-        result += `
-        <tr>
-        <td>${i+1}</td>
-        <td>${c.title}</td>
-        <td>${c.price}</td>
-        <td class="qty-btn">
-            <button type="button" id="dec" class="dec-btn" data-id="${c.id}"> - </button>
-            <span>${c.quantity}</span>
-            <button type="button" id="inc" class="inc-btn" data-id="${c.id}"> + </button>
-        </td>
-        <td><button type="button" class="remove-btn" data-id="${c.id}">Remove</button></td>
-        </tr>
-  `;
-  
-   revList.innerHTML = result; 
-  //sum(items);
-  
-  sum(items);
-  
-        // actionBtns = document.querySelectorAll('.action-btns');
-        // removeBtns = document.querySelectorAll(".remove-btn");
-        // qtyBtns = document.getElementById('qty-btn');
-        // removeArr = Array.from(removeBtns);
-        // actionBtnsArr = Array.from(actionBtns);
-  })
 }
 
 //Remove Cart Items From DOM and Update Price
 function removeFromDom(){
     revList.addEventListener('click', function(e){
         if(e.target.className == 'remove-btn'){
-            let row = e.target.parentElement.parentElement; 
+            var row = e.target.parentElement.parentElement; 
             revList.removeChild(row);
-            let toRemove = myCart.filter(prod=>prod.id == e.target.dataset.id);
-            
-            let btnToRem = [...row.children][4].children[0].dataset.id
-
-            let actualBtn = btns_arr.find(bt => bt.dataset.id == btnToRem);
-            console.log(actualBtn);
-            actualBtn.style.backgroundColor = '#FF9A3D';
-            actualBtn.textContent = 'ADD TO CART';
-
-
+            var toRemove = myCart.filter(prod=>prod.id == e.target.dataset.id);
+            console.log(toRemove[0]);
             removeFromCart(toRemove[0]);
-        } else if(e.target.className == 'dec-btn'){
-            
-            var decQty = myCart.filter(cat => cat.id == parseInt(e.target.dataset.id));
-            console.log(decQty[0].quantity - 1);
-        }else if(e.target.className == 'inc-btn'){
-            var incQty = myCart.filter(cat => cat.id == parseInt(e.target.dataset.id));
-            
-        }
+        }   
     })
 }
 
@@ -243,6 +178,16 @@ function sum(arr){
     total.innerHTML = sum;
     document.getElementById('num').innerHTML = arr.length;
     return sum;
+}
+
+//Increase Quantity
+function inc(){
+  revList.addEventListener('click', function(e){
+    //   if(e.target.className == ){
+
+    //   }
+    console.log(e.target);
+  })
 }
 
 //Decrease Quantity
@@ -267,14 +212,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
     backToShop.addEventListener('click', continueShopping);
 
-    btns_arr.forEach(function(btn){
-        var prod = products.find(p => p.id == parseInt(btn.dataset.id));
-        btn.addEventListener('click', ()=>addToCart(prod,btn));
+    btns.forEach(function(btn){
+        var prod = products.filter(p => p.id == parseInt(btn.dataset.id));
+        btn.addEventListener('click', ()=>addToCart(prod[0],btn));
     }); 
 
-    // removeArr.forEach(function(btn){
-    //     var prod = myCart.filter(p => p.id == parseInt(btn.dataset.id));
-    //     btn.addEventListener('click', ()=>removeFromCart(prod[0]));
-    // }); 
+    removeArr.forEach(function(btn){
+        var prod = myCart.filter(p => p.id == parseInt(btn.dataset.id));
+        btn.addEventListener('click', ()=>removeFromCart(prod[0]));
+    }); 
 
+  
+     
 })
